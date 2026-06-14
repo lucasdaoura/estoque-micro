c268812ea6834d65868f7f5138598ae3b6ea5e1a
lucasdaoura-star <lucas.daoura123@gmail.com>
2026-06-14 19:03:00 -0300

    fix: corrigir estrutura de pastas

--- estoque-micro/src/main/java/br/com/lucasdaoura/estoquemicro/exception/ProdutoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.lucasdaoura.estoquemicro.exception;
-
-public class ProdutoNotFoundException extends RuntimeException {
-    public ProdutoNotFoundException(String message) {
-        super(message);
-    }
-}
--- estoque-micro/src/main/java/br/com/lucasdaoura/estoquemicro/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lucasdaoura.estoquemicro.repository;
-
-import br.com.lucasdaoura.estoquemicro.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
